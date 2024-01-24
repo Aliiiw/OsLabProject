353cc63a6d45332da125725f1f3857bef42d316f
Ali Rahimi <1aliirahimi1@gmail.com>
2024-01-24 20:06:35 +0330

    q1 done

--- osLabProject/src/Main.java
+++ osLabProject/src/Main.java
@@ -1,5 +1,50 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+    private static int iteration = 1;
+    private static double efficiencyRatio = 0;
+
+    public static void main(String[] args) throws InterruptedException {
+
+        while (efficiencyRatio <= 2) {
+            //start from 100000 according to Documentation
+            int count = (int) Math.pow(2, iteration) * 100000;
+            long[] dataSet = createAndFill(count);
+
+            long startTimeSerial = System.currentTimeMillis();
+            long maxSerial = findPeakSequentially(dataSet);
+            long endTimeSerial = System.currentTimeMillis();
+            long serialDuration = endTimeSerial - startTimeSerial;
+
+            long startTimeParallel = System.currentTimeMillis();
+            long maxParallel = Concurrent.executeConcurrently(dataSet);
+            long endTimeParallel = System.currentTimeMillis();
+            long parallelDuration = endTimeParallel - startTimeParallel;
+
+            efficiencyRatio = (double) serialDuration / parallelDuration;
+
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.append("Iteration: ").append(iteration).append("\n");
+            stringBuilder.append("Count: ").append(count).append("\n");
+            stringBuilder.append("Serial Max: ").append(maxSerial).append("\n");
+            stringBuilder.append("Serial Time: ").append(serialDuration).append("ms").append("\n");
+            stringBuilder.append("Parallel Max: ").append(maxParallel).append("\n");
+            stringBuilder.append("Parallel Time: ").append(parallelDuration).append("ms").append("\n");
+            stringBuilder.append("Efficiency Ratio: ").append(efficiencyRatio).append("\n");
+            stringBuilder.append("----------------------------------------------------------");
+            System.out.println(stringBuilder);
+            iteration++;
+        }
+    }
+
+    private static long findPeakSequentially(long[] dataset) {
+        return java.util.Arrays.stream(dataset).max().orElse(Long.MIN_VALUE);
+    }
+
+    private static long[] createAndFill(int count) {
+        long[] dataSet = new long[count];
+        for (int i = 0; i < count; i++) {
+            dataSet[i] = (long) (Math.random() * Long.MAX_VALUE);
+        }
+        return dataSet;
     }
 }
